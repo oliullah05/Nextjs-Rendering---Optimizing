6264ed81ad6e178e1a1d007f3471727ca7fa40d3
oliullah05 <mdolihasan@gmail.com>
2024-05-25 14:19:18 +0600

    Composition Patterns

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
             <body className={inter.className}>
                 <Header />
                 {children}
+                
             </body>
         </html>
     );
