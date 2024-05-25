0920a19f3675773a56b5d166bf0d01b196325082
oliullah05 <mdolihasan@gmail.com>
2024-05-25 16:17:55 +0600

    Image Optimization

--- app/page.js
+++ app/page.js
@@ -1,12 +1,23 @@
+import devinImage from "@/public/assets/images/devin.jpg";
+import Image from "next/image";
+import { blurDataURL } from "@/public/image/blurImageUrl";
+
 export default function Home() {
     return (
         <div>
-            <h1>Next.js Rendering process</h1>
-            <div className="mt-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
-                aperiam rerum expedita soluta cupiditate maiores excepturi
-                exercitationem, veniam corrupti praesentium pariatur quas dolor
-                iste dolores! Magni, adipisci excepturi. Nam, harum.
+            <h1>Next.js Image Optimization</h1>
+
+            <div className="mt-4 relative w-[800px] h-[400px] border-2 border-teal-300 overflow-hidden">
+                <Image
+                    placeholder="blur"
+                    fill
+                    priority={false}
+                    quality={75}
+                    src="https://img.youtube.com/vi/NgrljB7UU34/maxresdefault.jpg"
+                    blurDataURL={blurDataURL}
+                    alt="Will devin replace Programmers?"
+                    className="object-cover"
+                />
             </div>
         </div>
     );
