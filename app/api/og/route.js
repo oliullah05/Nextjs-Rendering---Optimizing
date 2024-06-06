d72ed5398083ba1b2246ebbc20283a2594183451
oliullah05 <mdolihasan@gmail.com>
2024-06-06 17:58:35 +0600

    dynamically og image create done

--- app/api/og/route.js
+++ app/api/og/route.js
@@ -1,24 +1,60 @@
 import { ImageResponse } from 'next/og'
  
-export const runtime = 'edge'
+// export const runtime = 'edge'
  
-export async function GET() {
+export async function GET(request) {
+
+const {searchParams} = new URL(request.url)
+const hasTitle = searchParams.has("title");
+const title = hasTitle?searchParams.get("title"):"my website"
+console.log({title});
   return new ImageResponse(
     (
-      <div
-      tw='text-7xl w-full h-full bg-red-200 flex justify-center items-center'
-        // style={{
-        //   fontSize: 128,
-        //   background: 'white',
-        //   width: '100%',
-        //   height: '100%',
-        //   display: 'flex',
-        //   textAlign: 'center',
-        //   alignItems: 'center',
-        //   justifyContent: 'center',
-        // }}
-      >
-        Hello world!
+        <div
+        style={{
+          display: 'flex',
+          height: '100%',
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          backgroundImage: 'linear-gradient(to bottom, #dbf4ff, #fff1f1)',
+          fontSize: 60,
+          letterSpacing: -2,
+          fontWeight: 700,
+          textAlign: 'center',
+        }}
+        >
+        <div
+          style={{
+            backgroundImage: 'linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))',
+            backgroundClip: 'text',
+            '-webkit-background-clip': 'text',
+            color: 'transparent',
+          }}
+        >
+         {title}
+        </div>
+        <div
+          style={{
+            backgroundImage: 'linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128))',
+            backgroundClip: 'text',
+            '-webkit-background-clip': 'text',
+            color: 'transparent',
+          }}
+        >
+          Preview
+        </div>
+        <div
+          style={{
+            backgroundImage: 'linear-gradient(90deg, rgb(255, 77, 77), rgb(249, 203, 40))',
+            backgroundClip: 'text',
+            '-webkit-background-clip': 'text',
+            color: 'transparent',
+          }}
+        >
+          Ship
+        </div>
       </div>
     ),
     {
